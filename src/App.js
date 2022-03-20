2040eaf0724b3889ec0ea4b89e6af3e9cb1ce2ca
dranik <dranik111100rs@gmail.com>
2022-03-20 20:49:05 +0300

    Adding event

--- src/App.js
+++ src/App.js
@@ -19,9 +19,16 @@ class App extends Component {
 
     const blocks = this.state.sameBlocks;
 
+    var changeTitleHandler = () => {
+      console.log("changeTitleHandler");
+    }
+
     return (
         <div className="App" style={divStyle}>
           <h3>{this.state.titlePage}</h3>
+
+          <button onClick={this.changeTitleHandler}>Change title</button>
+
             <SameBlock number={blocks[0].number} name={blocks[0].name} message={blocks[0].message}/>
             <SameBlock number={blocks[1].number} name={blocks[1].name} message={blocks[1].message}/>
             <SameBlock number={blocks[2].number} name={blocks[2].name} message={blocks[2].message}/>
