33bc8fb9e01b22acebfaffe9a0755008d388549f
dranik <dranik111100rs@gmail.com>
2022-03-28 00:25:39 +0300

    Dynamic classes

--- src/SameBlock/SameBlock.js
+++ src/SameBlock/SameBlock.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './SomeBlock.css';
 
-export default (props) => (
-    <div className='SomeBlock'
-         // style={{
-         //     border: '1px solid #ccc',
-         //     marginBottom: '10px',
-         //     display: 'block',
-         //     padding: '10px',
-         //     boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
-         //     borderRadius: '5px'
-         // }}
-    >
-        <div>{props.number}: <strong>{props.name}</strong> {props.message}</div>
-        <input type='text' onChange={props.onChangeName} value={props.name}/>
-        <button onClick={props.onDelete}>X</button>
-    </div>
-);
+export default (props) => {
+    const inputClasses = ['input']
+
+    if (props.name !== '') {
+        inputClasses.push('green');
+    } else {
+        inputClasses.push('red');
+    }
+
+    if (props.name.length > 4){
+        inputClasses.push('bold');
+    }
+
+    return (
+        <div className='SomeBlock'>
+            <div>{props.number}: <strong>{props.name}</strong> {props.message}</div>
+            <input type='text' className={inputClasses.join(' ')} onChange={props.onChangeName} value={props.name}/>
+            <button onClick={props.onDelete}>X</button>
+        </div>
+    )
+}
