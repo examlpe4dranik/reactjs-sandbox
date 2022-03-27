529fc580694d02b1b76a2e93056324d21e091cf9
dranik <dranik111100rs@gmail.com>
2022-03-28 00:57:54 +0300

    Radium

--- src/SameBlock/SameBlock.js
+++ src/SameBlock/SameBlock.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Radium from 'radium';
 import './SomeBlock.css';
 
-export default (props) => {
+const someBlock = (props) => {
     const inputClasses = ['input']
 
     if (props.name !== '') {
@@ -14,11 +15,25 @@ export default (props) => {
         inputClasses.push('bold');
     }
 
+    const focusStyle = {
+        border: '1px solid #ccc',
+        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+        borderRadius: '5px',
+        ':hover': {
+            border: '1px solid #aaa',
+            boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
+            transition: 'border, box-shadow .3s',
+            cursor: 'pointer'
+        }
+    }
+
     return (
-        <div className='SomeBlock'>
+        <div className='SomeBlock' style={focusStyle}>
             <div>{props.number}: <strong>{props.name}</strong> {props.message}</div>
             <input type='text' className={inputClasses.join(' ')} onChange={props.onChangeName} value={props.name}/>
             <button onClick={props.onDelete}>X</button>
         </div>
     )
 }
+
+export default Radium(someBlock)
