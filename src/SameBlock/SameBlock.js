79ab96a3b91dc75d414ac017a2d0326d01251728
dranik <dranik111100rs@gmail.com>
2022-05-02 16:23:22 +0300

    Create Stateful component

--- src/SameBlock/SameBlock.js
+++ src/SameBlock/SameBlock.js
@@ -2,38 +2,40 @@ import React from 'react';
 import Radium from 'radium';
 import './SomeBlock.scss';
 
-const someBlock = (props) => {
-    const inputClasses = ['input']
+class SameBlock extends React.Component {
+    render() {
+        const inputClasses = ['input']
 
-    if (props.name !== '') {
-        inputClasses.push('green');
-    } else {
-        inputClasses.push('red');
-    }
-
-    if (props.name.length > 4){
-        inputClasses.push('bold');
-    }
-
-    const focusStyle = {
-        border: '1px solid #ccc',
-        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
-        borderRadius: '5px',
-        ':hover': {
-            border: '1px solid #aaa',
-            boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
-            transition: 'border, box-shadow .3s',
-            cursor: 'pointer'
+        if (this.props.name !== '') {
+            inputClasses.push('green');
+        } else {
+            inputClasses.push('red');
         }
-    }
 
-    return (
-        <div className='SomeBlock' style={focusStyle}>
-            <div>{props.number}: <strong>{props.name}</strong> {props.message}</div>
-            <input type='text' className={inputClasses.join(' ')} onChange={props.onChangeName} value={props.name}/>
-            <button onClick={props.onDelete}>X</button>
-        </div>
-    )
+        if (this.props.name.length > 4){
+            inputClasses.push('bold');
+        }
+
+        const focusStyle = {
+            border: '1px solid #ccc',
+            boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+            borderRadius: '5px',
+            ':hover': {
+                border: '1px solid #aaa',
+                boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
+                transition: 'border, box-shadow .3s',
+                cursor: 'pointer'
+            }
+        }
+
+        return (
+            <div className='SomeBlock' style={focusStyle}>
+                <div>{this.props.number}: <strong>{this.props.name}</strong> {this.props.message}</div>
+                <input type='text' className={inputClasses.join(' ')} onChange={this.props.onChangeName} value={this.props.name}/>
+                <button onClick={this.props.onDelete}>X</button>
+            </div>
+        )
+    }
 }
 
-export default Radium(someBlock)
+export default Radium(SameBlock)
